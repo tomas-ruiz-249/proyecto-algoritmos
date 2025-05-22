77fdb88181a20ad3be57c0b30e1f0e8b7d4cb572
tomas <tomas.ruizcorrea@gmail.com>
2025-05-21 22:29:44 -0500

    spanish docs

--- docs/html/menudata.js
+++ docs/html/menudata.js
@@ -23,4 +23,4 @@
  @licend  The above is the entire license notice for the JavaScript code in this file
 */
 var menudata={children:[
-{text:"Main Page",url:"index.html"}]}
+{text:"Página principal",url:"index.html"}]}
